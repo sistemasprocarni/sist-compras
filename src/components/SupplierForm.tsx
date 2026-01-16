7bc03dc5074f5b7787fd26f3ab5c75062a62dffc
[dyad] <sistemasprocarni2025@gmail.com>
2026-01-16 14:33:47 -0400

    Reverted all changes back to version 8877731797d331d7292dbb7044f62d0067f3f2d8

--- src/components/SupplierForm.tsx
+++ src/components/SupplierForm.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form'; // Corregido: de '@hookform/react' a 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 import { Button } from '@/components/ui/button';
@@ -106,10 +106,7 @@ const SupplierForm: React.FC<SupplierFormProps> = ({ initialData, onSubmit, onCa
       finalPaymentTerms = data.custom_payment_terms;
     }
 
-    // Crear un nuevo objeto de datos para enviar, excluyendo custom_payment_terms
-    const { custom_payment_terms, ...dataToSubmit } = data;
-
-    onSubmit({ ...dataToSubmit, rif: normalizedRif, payment_terms: finalPaymentTerms });
+    onSubmit({ ...data, rif: normalizedRif, payment_terms: finalPaymentTerms });
   };
 
   return (
