4fb36034032eec2ca402b8d4c2317d120b4d497a
[dyad] <sistemasprocarni2025@gmail.com>
2026-01-19 10:36:51 -0400

    [dyad] wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
+import BulkUpload from "./pages/BulkUpload"; // Importar el componente BulkUpload
 import { SessionContextProvider } from "./components/SessionContextProvider";
 
 const queryClient = new QueryClient();
@@ -19,7 +20,7 @@ const App = () => (
         <SessionContextProvider>
           <Routes>
             <Route path="/login" element={<Login />} />
-            <Route path="/" element={<Index />} />
+            <Route path="/" element={<BulkUpload />} /> {/* Cambiar la ruta principal a BulkUpload */}
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
