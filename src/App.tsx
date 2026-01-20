6958844314d731306803d7b153a3cb3f1ff5b57a
[dyad] <sistemasprocarni2025@gmail.com>
2026-01-20 09:29:55 -0400

    Reverted all changes back to version 27f4c4fe7d551f22a4876779c5962a8049934136

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,18 @@
 "use client";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
-import { SessionContextProvider } from "./components/SessionContextProvider";
-import Layout from "./components/Layout";
-import Login from "./pages/Login";
-import NotFound from "./pages/NotFound";
 import Index from "./pages/Index";
-import SearchSuppliersByMaterial from "./pages/SearchSuppliersByMaterial";
-import SupplierManagement from "./pages/SupplierManagement";
-import SupplierDetails from "./pages/SupplierDetails";
-import MaterialManagement from "./pages/MaterialManagement";
-import GenerateQuoteRequest from "./pages/GenerateQuoteRequest";
-import QuoteRequestManagement from "./pages/QuoteRequestManagement";
-import QuoteRequestDetails from "./pages/QuoteRequestDetails";
-import EditQuoteRequest from "./pages/EditQuoteRequest";
-import GeneratePurchaseOrder from "./pages/GeneratePurchaseOrder";
-import BulkUpload from "./pages/BulkUpload";
-import CompanyManagement from "./pages/CompanyManagement"; // Import CompanyManagement
+import SearchSuppliersByMaterial from "./pages/SearchSuppliersByMaterial"; // Import the new page
+import { Toaster } from "react-hot-toast"; // Assuming react-hot-toast is used for toasts
 
 function App() {
   return (
     <Router>
-      <Toaster />
-      <SessionContextProvider>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Index />} />
-            <Route path="/company-management" element={<CompanyManagement />} /> {/* New route for Company Management */}
-            <Route path="/search-suppliers-by-material" element={<SearchSuppliersByMaterial />} />
-            <Route path="/supplier-management" element={<SupplierManagement />} />
-            <Route path="/suppliers/:id" element={<SupplierDetails />} />
-            <Route path="/material-management" element={<MaterialManagement />} />
-            <Route path="/generate-quote" element={<GenerateQuoteRequest />} />
-            <Route path="/quote-request-management" element={<QuoteRequestManagement />} />
-            <Route path="/quote-requests/:id" element={<QuoteRequestDetails />} />
-            <Route path="/quote-requests/edit/:id" element={<EditQuoteRequest />} />
-            <Route path="/generate-po" element={<GeneratePurchaseOrder />} />
-            <Route path="/bulk-upload" element={<BulkUpload />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </SessionContextProvider>
+      <Toaster /> {/* Add Toaster for notifications */}
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/search-suppliers-by-material" element={<SearchSuppliersByMaterial />} /> {/* New route */}
+      </Routes>
     </Router>
   );
 }
