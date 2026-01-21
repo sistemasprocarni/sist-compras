f677add7933717fc044d782eb46c08dcf031499b
[dyad] <sistemasprocarni2025@gmail.com>
2026-01-21 09:41:57 -0400

    [dyad] wrote 1 file(s)

--- src/components/SupplierForm.tsx
+++ src/components/SupplierForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from 'react';
-import { useForm } from '@hookform/react-hook-form';
+import { useForm } from 'react-hook-form'; // Corregido: importación directa desde 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 import { Button } from '@/components/ui/button';
