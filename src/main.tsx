59e63327f6bf40eaac7355673531579c823c7816
[dyad] <sistemasprocarni2025@gmail.com>
2026-01-22 09:48:11 -0400

    Reverted all changes back to version fe50b82da913a26fd05410fab4fe056e2acda842

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/*",
-      element: <App />,
-    },
-  ],
-  {
-    future: {
-      v7_startTransition: true,
-      v7_relativeSplatPath: true,
-    },
-  }
-);
-
-createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
-);
+createRoot(document.getElementById("root")!).render(<App />);
